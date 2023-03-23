78d5ad2b5057781c6728cb3b16580950a1863de9
zineb <zinebaf96@gmail.com>
2023-03-23 16:52:08 +0100

    Estado inicial del ejercicio

---
+++ src/main/java/com/example/SpringMvCcRuDempresaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvCcRuDempresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvCcRuDempresaApplication.class, args);
+	}
+
+}
